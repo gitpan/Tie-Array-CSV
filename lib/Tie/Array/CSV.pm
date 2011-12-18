96f61d02debf7ae1333f6ff7ac2d01d9df1e924c
Joel Berger <joel.a.berger@gmail.com>
2011-12-18 18:15:17 +0000

    Import of JBERGER/Tie-Array-CSV-0.04 from CPAN.
    gitpan-cpan-distribution: Tie-Array-CSV
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         JBERGER/Tie-Array-CSV-0.04.tar.gz
    gitpan-cpan-author:       JBERGER
    gitpan-cpan-maturity:     released

--- lib/Tie/Array/CSV.pm
+++ lib/Tie/Array/CSV.pm
@@ -1,6 +1,6 @@
 package Tie::Array::CSV;
 BEGIN {
-  $Tie::Array::CSV::VERSION = '0.03';
+  $Tie::Array::CSV::VERSION = '0.04';
 }
 
 use strict;
@@ -14,13 +14,11 @@ use Text::CSV;
 use Tie::Array;
 our @ISA = ('Tie::Array');
 
-sub new {
-  my $class = shift;
+sub parse_opts {
 
   croak "Must specify a file" unless @_;
 
-  my $file;
-  my %opts;
+  my ($file, %opts);
 
   # handle one arg as either hashref (of opts) or file
   if (@_ == 1) {
@@ -55,7 +53,19 @@ sub new {
   # file wasn't specified as lone arg or as a hash opt
   croak "Must specify a file" unless $file;
 
-  tie my @self, __PACKAGE__, $file, \%opts;
+  # parse specific options
+  if (exists $opts{sep_char}) {
+    $opts{text_csv}{sep_char} = delete $opts{sep_char};
+  }
+
+  return ($file, \%opts);
+}
+
+sub new {
+  my $class = shift;
+  my ($file, $opts) = parse_opts(@_);
+
+  tie my @self, __PACKAGE__, $file, $opts;
 
   return \@self;
 
@@ -63,7 +73,7 @@ sub new {
 
 sub TIEARRAY {
   my $class = shift;
-  my ($file, $opts) = @_;
+  my ($file, $opts) = parse_opts(@_);
 
   my @tiefile;
   tie @tiefile, 'Tie::File', $file, %{ $opts->{tie_file} || {} }
@@ -75,6 +85,7 @@ sub TIEARRAY {
   my $self = {
     file => \@tiefile,
     csv => $csv,
+    hold_row => (defined $opts->{hold_row} ? $opts->{hold_row} : 1),
   };
 
   bless $self, $class;
@@ -97,6 +108,7 @@ sub FETCH {
     line_num => $index,
     fields => \@fields, 
     csv => $self->{csv},
+    hold => $self->{hold_row},
   };
 
   return \@line;
@@ -129,7 +141,7 @@ sub STORESIZE {
 
 package Tie::Array::CSV::Row;
 BEGIN {
-  $Tie::Array::CSV::Row::VERSION = '0.03';
+  $Tie::Array::CSV::Row::VERSION = '0.04';
 }
 
 use Carp;
@@ -145,6 +157,8 @@ sub TIEARRAY {
   my $self = shift;
 
   bless $self, $class;
+
+  $self->{need_update} = 0;
 
   return $self;
 }
@@ -162,7 +176,11 @@ sub STORE {
 
   $self->{fields}[$index] = $value;
 
-  $self->_update;
+  if ($self->{hold}) {
+    $self->{need_update} = 1;
+  } else {
+    $self->_update;
+  }
 
 }
 
@@ -180,7 +198,11 @@ sub STORESIZE {
     $#{ $self->{fields} } = $new_size - 1
   );
 
-  $self->_update;
+  if ($self->{hold}) {
+    $self->{need_update} = 1;
+  } else {
+    $self->_update;
+  }
 
   return $return;
 }
@@ -190,7 +212,11 @@ sub SHIFT {
 
   my $value = shift @{ $self->{fields} };
 
-  $self->_update;
+  if ($self->{hold}) {
+    $self->{need_update} = 1;
+  } else {
+    $self->_update;
+  }
 
   return $value;
 }
@@ -201,7 +227,11 @@ sub UNSHIFT {
 
   unshift @{ $self->{fields} }, $value;
 
-  $self->_update;
+  if ($self->{hold}) {
+    $self->{need_update} = 1;
+  } else {
+    $self->_update;
+  }
 
   return $self->FETCHSIZE();
 }
@@ -212,6 +242,11 @@ sub _update {
   $self->{csv}->combine(@{ $self->{fields} })
     or croak "CSV combine error: " . $self->{csv}->error_diag();
   $self->{file}[$self->{line_num}] = $self->{csv}->string;
+}
+
+sub DESTROY {
+  my $self = shift;
+  $self->_update if $self->{need_update} == 1;
 }
 
 __END__
@@ -275,25 +310,32 @@ uses the speedy L<Text::CSV_XS> if installed
 
 This module was inspired by L<Tie::CSV_File> which (sadly) hasn't been maintained. It also doesn't attempt to do any of the parsing (as that module did), but rather passes all of the heavy lifting to other modules.
 
+Note that while the L<Tie::File> prevents the need to read in the entire file, while in use, a parsed row IS held in memory. This is true whether <hold_row> is in effect or not (see L<Options> below).
+
 =head1 CONSTRUCTORS
+
+Since version 0.04 both constructors allow the options that version 0.03 only offered for the C<new> constructor. The constructors must be passed a file name, either as the first argument, or as the value to the option key C<file>. Options may be passed as key-value pairs or as a hash reference. This yields the many ways of calling the constructors shown below, one for every taste.
+
+N.B. Should a lone argument filename and a C<file> option key both be passed to the constructor, the lone argument wins.
 
 =head2 C<tie> Constructor
 
-As with any tied array, the construction uses the C<tie> function. 
+As with any tied array, the construction uses the C<tie> function. Basic usage is as follows:
 
  tie my @file, 'Tie::Array::CSV', 'filename';
 
-would tie the lexically scoped array C<@file> to the file C<filename> using this module. Following these three arguements to C<tie>, one may optionally pass a hashref containing additional configuration.
+which would tie the lexically scoped array C<@file> to the file C<filename> using this module. Following the first two arguements to C<tie>, one may optionally pass a key-value pairs or a hashref containing additional configuration or even file specification.
 
  tie my @file, 'Tie::Array::CSV', 'filename', { opt_key => val, ... };
+ tie my @file, 'Tie::Array::CSV', 'filename', opt_key => val, ... ;
+ tie my @file, 'Tie::Array::CSV', { file => 'filename', opt_key => val, ... };
+ tie my @file, 'Tie::Array::CSV', file => 'filename', opt_key => val, ... ;
 
 Of course, the magical Perl C<tie> can be scary for some, for those people there is the ...
 
 =head2 C<new> Constructor
 
 [ Added in version 0.03 ]
-
-The class method C<new> constructor is more flexible in its calling. The constructor must be passed a file name, either as the first argument, or as the value to the option key C<file>. Options may be passed as key-value pairs or as a hash reference. This yields the many ways of calling C<new> shown below, one for every taste.
 
  my $array = Tie::Array::CSV->new( 'filename' );
  my $array = Tie::Array::CSV->new( 'filename', { opt_key => val, ... });
@@ -303,30 +345,41 @@ The class method C<new> constructor is more flexible in its calling. The constru
 
 It only returns a reference to the C<tie>d array due to a limitations in how C<tie> magic works. 
 
-N.B. Should a lone argument filename and a C<file> option key both be passed to the constructor, the lone argument wins.
-
 =head2 Options
-
-Currently the only options are "pass-through" options, sent to the constructors of the different modules used internally, read more about them in those module's documentation.
 
 =over
 
 =item *
 
-tie_file - hashref of options which are passed to the L<Tie::File> constructor
+C<file> - alternative method for specifing the file to C<tie>. This is overridden by a lone filename or handle passed as the first argument to the constructor.
 
 =item *
 
-text_csv - hashref of options which are passed to the L<Text::CSV> constructor
+C<tie_file> - hashref of options which are passed to the L<Tie::File> constructor
+
+=item *
+
+C<text_csv> - hashref of options which are passed to the L<Text::CSV> constructor
+
+=item *
+
+C<sep_char> - for ease of use, a C<sep_char> option may be specified, which is passed to the L<Text::CSV> constructor. This option overrides a corresponding entry in the C<text_csv> pass-through hash.
+
+=item *
+
+C<hold_row> - If true, the file is not updated while the reference to the row is still in scope. The default is true. Note: that when false, the parsed row is still held in memory while the row is in scope, the ONLY difference is that the file reflects changes immediately when C<hold_row> is false. To reiterate, this option only affects file IO, not memory usage.
 
 =back
 
-example:
+Equivalent examples:
 
  tie my @file, 'Tie::Array::CSV', 'filename', { 
    tie_file => {}, 
    text_csv => { sep_char => ';' },
+   hold_row => 0
  };
+
+ tie my @file, 'Tie::Array::CSV', 'filename', sep_char => ';', hold_row => 0;
 
 =head1 ERRORS
 
@@ -342,7 +395,7 @@ Much of the functionality of normal arrays is mimicked using L<Tie::Array>. The 
 
 =item *
 
-Some effort had been made to allow for fields which contain linebreaks. Linebreaks would change line numbers used for row access by L<Tie::File>. This, unfortunately, moved the module far from its stated goals, and therefore far less powerful for its intended purposes. The decsion has been made (for now) not to support such files.
+Some effort had been made to allow for fields which contain linebreaks. Linebreaks would change line numbers used for row access by L<Tie::File>. This, unfortunately, moved the module far from its stated goals, and therefore far less powerful for its intended purposes. The decision has been made (for now) not to support such files.
 
 =back
 
